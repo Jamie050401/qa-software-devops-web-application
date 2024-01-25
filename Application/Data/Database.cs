5232e706a14b9cce7623882d715f403264843a91
Jamie050401 <jamieallen050401@gmail.com>
2024-01-25 13:53:39 +0000

    Small reorder of functions

--- Application/Data/Database.cs
+++ Application/Data/Database.cs
@@ -20,37 +20,6 @@ public class Database
 
     private string? ConnectionString { get; }
 
-    private void CreateTables()
-    {
-        const string sqlCreateTables = """
-                                          CREATE TABLE IF NOT EXISTS Funds (
-                                              Id INTEGER PRIMARY KEY NOT NULL,
-                                              Name TEXT NOT NULL,
-                                              GrowthRate REAL NOT NULL,
-                                              Charge REAL NOT NULL
-                                          );
-                                       
-                                          CREATE TABLE IF NOT EXISTS Roles (
-                                              Name TEXT PRIMARY KEY NOT NULL
-                                          );
-                                       
-                                          CREATE TABLE IF NOT EXISTS Users (
-                                              Id INTEGER PRIMARY KEY NOT NULL,
-                                              Username TEXT NOT NULL,
-                                              Password TEXT NOT NULL,
-                                              FirstName TEXT,
-                                              LastName TEXT,
-                                              RoleName TEXT NOT NULL,
-                                       
-                                              FOREIGN KEY(RoleName) REFERENCES Roles(Name) ON DELETE NO ACTION
-                                          );
-                                          
-                                          CREATE UNIQUE INDEX IF NOT EXISTS idx_Username ON Users(Username);
-                                       """;
-
-        this.ExecuteNonQuery(sqlCreateTables);
-    }
-
     // TODO - Implement local caching
     public ActionResult<Fund> GetFundFromDatabase(int fundId)
     {
@@ -71,15 +40,26 @@ public class Database
         return fund;
     }
 
-    // ReSharper disable once MemberCanBePrivate.Global
+    public ActionResult<int> DeleteFundFromDatabase(int fundId)
+    {
+        var fundInDb = this.GetFundFromDatabase(fundId);
+
+        if (fundInDb.Result == new NotFoundResult())
+        {
+            return new NotFoundResult();
+        }
+
+        var sqlDeleteFromDatabase = $"DELETE FROM Funds WHERE Id = {fundId}";
+        return this.ExecuteNonQuery(sqlDeleteFromDatabase);
+    }
+
     public int AddFundToDatabase(Fund fund)
     {
         var fundInDb = this.GetFundFromDatabase(fund.Id);
 
         if (fundInDb.Result != new NotFoundResult())
         {
-            // TODO - Implement logic to delete and add new item in database
-            return 0;
+            this.DeleteFundFromDatabase(fund.Id);
         }
 
         var sqlAddToDatabase = $"""
@@ -129,14 +109,45 @@ public class Database
         }
 
         var id = dbResponse.Reader.GetInt32(0);
-        var username = (string)dbResponse.Reader.GetString(1);
-        var password = (string)dbResponse.Reader.GetString(2);
+        var username = dbResponse.Reader.GetString(1);
+        var password = dbResponse.Reader.GetString(2);
         var firstName = dbResponse.Reader.IsDBNull(3) ? null : dbResponse.Reader.GetString(3);
         var lastName = dbResponse.Reader.IsDBNull(4) ? null : dbResponse.Reader.GetString(4);
         var roleName = dbResponse.Reader.GetString(5);
         dbResponse.Dispose();
         var user = new User(id, username, password, firstName, lastName, roleName);
         return user;
+    }
+
+    private void CreateTables()
+    {
+        const string sqlCreateTables = """
+                                          CREATE TABLE IF NOT EXISTS Funds (
+                                              Id INTEGER PRIMARY KEY NOT NULL,
+                                              Name TEXT NOT NULL,
+                                              GrowthRate REAL NOT NULL,
+                                              Charge REAL NOT NULL
+                                          );
+                                       
+                                          CREATE TABLE IF NOT EXISTS Roles (
+                                              Name TEXT PRIMARY KEY NOT NULL
+                                          );
+                                       
+                                          CREATE TABLE IF NOT EXISTS Users (
+                                              Id INTEGER PRIMARY KEY NOT NULL,
+                                              Username TEXT NOT NULL,
+                                              Password TEXT NOT NULL,
+                                              FirstName TEXT,
+                                              LastName TEXT,
+                                              RoleName TEXT NOT NULL,
+                                       
+                                              FOREIGN KEY(RoleName) REFERENCES Roles(Name) ON DELETE NO ACTION
+                                          );
+                                          
+                                          CREATE UNIQUE INDEX IF NOT EXISTS idx_Username ON Users(Username);
+                                       """;
+
+        this.ExecuteNonQuery(sqlCreateTables);
     }
 
     private int ExecuteNonQuery(string sqlCommand)
