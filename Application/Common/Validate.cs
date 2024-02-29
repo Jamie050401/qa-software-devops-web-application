42783c8916da5c6c1c8adef67b82405ba3fe6aa7
Jamie050401 <jamieallen050401@gmail.com>
2024-02-29 16:31:32 +0000

    Added unit tests for common validation logic

--- Application/Common/Validate.cs
+++ Application/Common/Validate.cs
@@ -8,31 +8,36 @@ public static partial class Validate
     [GeneratedRegex(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase, "en-GB")]
     private static partial Regex EmailRegex();
 
-    public static bool Email(INotyfService notyf, string email)
+    public static bool Email(INotyfService? notyf, string email)
     {
         var isValid = EmailRegex().IsMatch(email);
-        if (!isValid) notyf.Error("Please make sure to enter a valid email address.");
+        if (!isValid) notyf?.Error("Please make sure to enter a valid email address.");
         return isValid;
     }
 
-    public static bool Password(INotyfService notyf, string password, string confirmPassword)
+    public static bool Password(INotyfService? notyf, string password, string confirmPassword)
     {
         var isValid = password == confirmPassword;
         if (!isValid)
         {
-            notyf.Error("Please ensure both passwords match.");
+            notyf?.Error("Please ensure both passwords match.");
             return isValid;
         }
 
         isValid = password.Length > 11;
-        // ReSharper disable once InvertIf
         if (!isValid)
         {
-            notyf.Error("Password must be at least 12 characters long.");
+            notyf?.Error("Password must be at least 12 characters long.");
             return isValid;
         }
 
-        // TODO - Enforce complexity requirements ...
+        isValid = password.Length < 41;
+        // ReSharper disable once InvertIf
+        if (!isValid)
+        {
+            notyf?.Error("Password cannot exceed 40 characters.");
+            return isValid;
+        }
 
         return isValid;
     }
--- Application/Tests/Validate.cs
+++ Application/Tests/Validate.cs
@@ -1,11 +1,88 @@
 ﻿namespace Application.Tests;
 
-// TODO - Implement unit tests for Common.Validate ...
-
+using Common;
+using Microsoft.AspNetCore.Identity;
 using NUnit.Framework;
 
 [TestFixture]
-public class ValidateEmail;
+public class ValidateEmail
+{
+    [Test]
+    public void ValidEmailAllowed()
+    {
+        const string email = "test@email.com";
+
+        var actual = Validate.Email(null, email);
+
+        Assert.That(actual, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void InvalidEmailDenied()
+    {
+        // ReSharper disable once StringLiteralTypo
+        const string email = "testemail.com";
+
+        var actual = Validate.Email(null, email);
+
+        Assert.That(actual, Is.EqualTo(false));
+    }
+}
 
 [TestFixture]
-public class ValidatePassword;
+public class ValidatePassword
+{
+    [Test]
+    public void ValidPasswordAllowed()
+    {
+        // ReSharper disable once StringLiteralTypo
+        const string password = "thisisavalidpassword!";
+
+        var actual = Validate.Password(null, password, password);
+
+        Assert.That(actual, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void MismatchedPasswordDenied()
+    {
+        // ReSharper disable once StringLiteralTypo
+        const string password = "thisisavalidpassword!";
+
+        var actual = Validate.Password(null, password, $"{password}!");
+
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void ShortPasswordDenied()
+    {
+        // ReSharper disable once StringLiteralTypo
+        const string password = "tooshort";
+
+        var actual = Validate.Password(null, password, password);
+
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void LongPasswordDenied()
+    {
+        var passwordOptions = new PasswordOptions
+        {
+            RequiredLength = 60,
+            RequiredUniqueChars = 8,
+            RequireDigit = true,
+            RequireLowercase = true,
+            RequireNonAlphanumeric = true,
+            RequireUppercase = true
+        };
+
+        // ReSharper disable once StringLiteralTypo
+        var password = Secret.Generate(passwordOptions);
+
+        var actual = Validate.Password(null, password, password);
+
+        Assert.That(actual, Is.EqualTo(false));
+    }
+}
