c878a1b3b81d62c2709b61b5183b39a678375e5a
Jamie050401 <jamieallen050401@gmail.com>
2024-03-01 11:31:50 +0000

    Replaced nav-link redirects for /dashboard and /privacy in the navigation bar

--- Application/Pages/Shared/PageModel.cs
+++ Application/Pages/Shared/PageModel.cs
@@ -2,6 +2,16 @@
 
 public class PageModel : Microsoft.AspNetCore.Mvc.RazorPages.PageModel
 {
+    public void OnPostDashboard()
+    {
+        Response.Redirect("/dashboard");
+    }
+
+    public void OnPostPrivacy()
+    {
+        Response.Redirect("/privacy");
+    }
+
     public void OnPostLogout()
     {
         Response.Redirect("/logout");
