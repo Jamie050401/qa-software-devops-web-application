cb287700f83a5d59b9b64a04c0f0aab15c4f416f
Jamie050401 <jamieallen050401@gmail.com>
2024-03-03 23:05:43 +0000

    Removed memory cache from database logic (will reintroduce if performance tests deem it necessary)

--- Application/Data/Database.cs
+++ Application/Data/Database.cs
@@ -2,7 +2,6 @@
 
 using Common;
 using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Caching.Memory;
 using Models;
 using Newtonsoft.Json;
 using System.Diagnostics;
@@ -118,11 +117,9 @@ public class Database
             return Response<IModel, Error>.BadRequestResponse($"Database query failed - {ex.Message}");
         }
 
-        if (createUpdateAffected <= 0)
-            return Response<IModel, Error>.NotFoundResponse($"{value.Id} does not exist within the database");
-
-        this.AddToCache(value);
-        return Response<IModel, Error>.OkResponse();
+        return createUpdateAffected <= 0
+            ? Response<IModel, Error>.NotFoundResponse($"{value.Id} does not exist within the database")
+            : Response<IModel, Error>.OkResponse();
     }
 
     private Response<IModel, Error> ReadDelete(OperationType operationType, Inputs inputs)
@@ -130,13 +127,6 @@ public class Database
         // ReSharper disable once ConvertIfStatementToSwitchStatement
         if (operationType is OperationType.Create or OperationType.Update)
             return Response<IModel, Error>.BadRequestResponse($"{operationType} is not supported");
-
-        if (operationType is OperationType.Read)
-        {
-            var valueInCache = this.GetFromCache($"{inputs.PropertyValue}");
-            if (valueInCache is not null)
-                return Response<IModel, Error>.OkValueResponse(valueInCache);
-        }
 
         var valueType = GetValueType(null, inputs);
         if (valueType is null)
@@ -176,7 +166,6 @@ public class Database
             {
                 GetDatabaseValue(valueInDb, property);
             }
-            this.AddToCache(valueInDb);
             return Response<IModel, Error>.OkValueResponse(valueInDb);
         }
 
@@ -199,7 +188,6 @@ public class Database
             return Response<IModel, Error>.NotFoundResponse($"{inputs.PropertyValue} does not exist within the database");
 
         Debug.Assert(dbResponse.Value != null, "dbResponse.Value != null");
-        this.DeleteFromCache(dbResponse.Value);
         return Response<IModel, Error>.OkResponse();
     }
 
@@ -411,47 +399,6 @@ public class Database
         return new ExecuteReaderResponse(dbConnection, dbCommand, dbCommand.ExecuteReader());
     }
 
-    private void EditCache(CacheBehaviour behaviour, IModel model)
-    {
-        var modelType = model.GetType();
-        var prefix = modelType.Name;
-
-        var modelProperties = modelType.GetProperties();
-        foreach (var property in modelProperties)
-        {
-            var key = $"{prefix}-{property.Name}";
-            switch (behaviour)
-            {
-                case CacheBehaviour.Add:
-                    Cache.Set(key, model, CacheEntryOptions);
-                    break;
-                case CacheBehaviour.Remove:
-                    Cache.Remove(key);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(behaviour), behaviour, null);
-            }
-        }
-    }
-
-    private void DeleteFromCache(IModel model)
-    {
-        this.EditCache(CacheBehaviour.Remove, model);
-    }
-
-    private void AddToCache(IModel model)
-    {
-        this.EditCache(CacheBehaviour.Add, model);
-    }
-
-    private IModel? GetFromCache(object? key)
-    {
-        if (key is null) return null;
-
-        Cache.TryGetValue(key, out var value);
-        return (IModel?)value;
-    }
-
     private readonly struct ExecuteReaderResponse(SqliteConnection connection, SqliteCommand command, SqliteDataReader reader)
     {
         public SqliteDataReader Reader { get; } = reader;
@@ -478,16 +425,5 @@ public class Database
         Delete
     }
 
-    private enum CacheBehaviour
-    {
-        Add,
-        Remove
-    }
-
     private string? ConnectionString { get; }
-    private MemoryCache Cache { get; } = new(new MemoryCacheOptions());
-    private MemoryCacheEntryOptions CacheEntryOptions { get; } = new()
-    {
-        SlidingExpiration = TimeSpan.FromMinutes(30)
-    };
 }
