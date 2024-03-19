62c913f98b1c3be0169e4d64aceea965414ed04e
Jamie050401 <jamieallen050401@gmail.com>
2024-03-19 15:47:04 +0000

    Enhanced calculation logic to perform an actual roll-up

--- Engine/Calculation.fs
+++ Engine/Calculation.fs
@@ -1,6 +1,7 @@
 ﻿module Engine.Calculation
 
 open Domain
+open System
 
 let Calculate (inputs : Inputs) =
     let negate originalValue currentValue =
@@ -16,9 +17,22 @@ let Calculate (inputs : Inputs) =
     let funds = inputs.Funds |>  Array.ofSeq
     let investmentPercentages = inputs.InvestmentPercentages |> Array.ofSeq |> Dictionary
 
-    funds
-    |> Array.Parallel.map (fun fund ->
-        investmentPercentages[fund.Id] * inputs.Investment * (1.0M + fund.GrowthRate) * (1.0M - fund.Charge))
+    funds |> Array.Parallel.map (fun fund ->
+        let now = DateTime.UtcNow
+        let future = now.AddYears(10)
+
+        let startMonth = now.Year * 12 + now.Month
+        let endMonth = future.Year * 12 + future.Month
+        let months = Array.init (endMonth - startMonth) (fun index -> index + startMonth)
+        months |> (investmentPercentages[fund.Id] / 100.0M * inputs.Investment |> Array.fold (fun fundValue totalMonth ->
+            let month =
+                let value = totalMonth % 12
+                match value with
+                | 0 -> 12
+                | _ -> value
+            let year = (totalMonth - month) / 12
+            let days = DateTime.DaysInMonth(year, month)
+            fundValue * decimal ((1.0 + float fund.GrowthRate) ** (float days / 365.25)) * decimal ((1.0 - float fund.Charge) ** (float days / 365.25)))))
     |> Array.sum
     |> float
     |> round 2
